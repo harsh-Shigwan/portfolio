5d87a77f28be6920b1009a1ca17a655a1a3a6cde
Harsh Shigwan <harshshigwan24@gmail.com>
2025-03-24 12:27:32 +0530

    add 2 project

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,7 +1,8 @@
 import Work1 from "../../assets/work1.png"
 import Work2 from "../../assets/work2.png"
 import Work3 from "../../assets/work3.png";
-
+import Work4 from "../../assets/work4.png";
+import Work5 from "../../assets/work5.png";
 export const projectsData = [
     {
       id: 1,
@@ -27,6 +28,21 @@ export const projectsData = [
        links : "https://brand-design-six.vercel.app/",
        gitLink:"https://github.com/harsh-Shigwan/Brand_Design"
     },
+    {
+      id: 4,
+      image: Work4,
+      title: "Music Player Saitify"
+      ,category: "web",
+      links : "https://music-player-saitify.netlify.app/",
+      gitLink:"https://github.com/harsh-Shigwan/Music_Player?tab=readme-ov-file"
+
+    },{
+      id:5,
+      image: Work5,
+      title: "blog",
+      category: "web",
+      gitLink:"https://github.com/harsh-Shigwan/Blogs.git"
+    }
    
   
   ];
